b9f8c1b07040cdd102fc6ebdefb642c59ae4d9a8
kaushal9678 <k.kaushal50@gmail.com>
2013-07-12 14:47:02 +0530

    Initial Commit

---
+++ JSONAlpha/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JSONAlpha
+//
+//  Created by kaushal on 12/07/13.
+//  Copyright (c) 2013 abc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
