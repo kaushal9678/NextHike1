3bd52ed8bcb24f4d07a0fcb94a15972b8679519e
kaushal9678 <k.kaushal50@gmail.com>
2013-07-12 16:09:14 +0530

    Added two Files Call_Service two Call web service

---
+++ JSONAlpha/Call_Service.h
@@ -0,0 +1,13 @@
+//
+//  Call_Service.h
+//  JSONAlpha
+//
+//  Created by kaushal on 12/07/13.
+//  Copyright (c) 2013 abc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Call_Service : NSString
+
+@end
---
+++ JSONAlpha/Call_Service.m
@@ -0,0 +1,13 @@
+//
+//  Call_Service.m
+//  JSONAlpha
+//
+//  Created by kaushal on 12/07/13.
+//  Copyright (c) 2013 abc. All rights reserved.
+//
+
+#import "Call_Service.h"
+
+@implementation Call_Service
+
+@end
