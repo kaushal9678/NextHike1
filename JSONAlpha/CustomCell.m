4a6e34c8c3feef670e352c446056ec21644e922d
kaushal9678 <k.kaushal50@gmail.com>
2013-07-12 15:56:05 +0530

    modify custom cell

--- JSONAlpha/CustomCell.m
+++ JSONAlpha/CustomCell.m
@@ -9,21 +9,37 @@
 #import "CustomCell.h"
 
 @implementation CustomCell
+@synthesize webView,label_Name,image_Avatar;
 
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
 {
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
-    if (self) {
-        // Initialization code
+    /*if (self) {
+     // Initialization code
+     
+     self = [[[NSBundle mainBundle] loadNibNamed:@"CustomCell" owner:self options:nil] objectAtIndex:0];
+     
+     }*/
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
+        // Running on iPhone or iPod touch
+        self=[[[NSBundle mainBundle] loadNibNamed:@"CustomCell" owner:self options:nil] objectAtIndex:0];
+        
+    } else {
+        // Running on iPad
+        self=[[[NSBundle mainBundle] loadNibNamed:@"CustomCell_iPad" owner:self options:nil] objectAtIndex:0];
     }
+    
+    self.selectionStyle=UITableViewCellSelectionStyleNone;
+    
     return self;
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated
 {
     [super setSelected:selected animated:animated];
-
+    
     // Configure the view for the selected state
 }
+
 
 @end
